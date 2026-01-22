09b7706fb705aa52ea5b7d5ed85eb0b1843c9419
rajmahesh446-cell <rajmahesh446@gmail.com>
2026-01-22 11:40:43 +0530

    Create Base64 Encoding (Standard Encoding).sol

---
+++ Base64 Encoding (Standard Encoding).sol
@@ -0,0 +1,16 @@
+import java.util.Base64;
+
+public class SimpleBase64 {
+    public static void main(String[] args) {
+        String originalInput = "JavaCrypto";
+        
+        // Encode
+        String encoded = Base64.getEncoder().encodeToString(originalInput.getBytes());
+        System.out.println("Encoded: " + encoded);
+        
+        // Decode
+        byte[] decodedBytes = Base64.getDecoder().decode(encoded);
+        String decoded = new String(decodedBytes);
+        System.out.println("Decoded: " + decoded);
+    }
+}
