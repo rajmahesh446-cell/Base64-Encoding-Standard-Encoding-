6968dd61b6ce2c565f33f7e3aa564031a19c57e6
rajmahesh446-cell <rajmahesh446@gmail.com>
2026-02-05 11:26:33 +0530

    Update Base64 Encoding (Standard Encoding).sol

--- Base64 Encoding (Standard Encoding).sol
+++ Base64 Encoding (Standard Encoding).sol
@@ -14,7 +14,7 @@ public class SimpleBase64 {
         System.out.println("Decoded: " + decoded);
     }
 }
-        System.out.println("Decoded: " + decoded);
+        
         
         
         
